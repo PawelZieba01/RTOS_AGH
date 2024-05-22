892e257e09afbaabd245e96964cfd9b15a3fbb80
PawelZieba01 <zpawel.zieba@gmail.com>
2024-05-22 16:18:09 +0200

    Skończony rozdział 2

--- Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -1,24 +1,42 @@
-#include <lpc21xx.h>
+#include "FreeRTOS.h"
+#include "task.h"
 #include "led.h"
-void Delay(unsigned int uiMiliSec)
-{
 
+void Delay(unsigned int uiMiliSec) 
+{
 	unsigned int uiLoopCtr, uiDelayLoopCount;
 	uiDelayLoopCount = uiMiliSec*12000;
-	for(uiLoopCtr=0;uiLoopCtr<uiDelayLoopCount;uiLoopCtr++) {}
+	for(uiLoopCtr=0;uiLoopCtr<uiDelayLoopCount;uiLoopCtr++){}
 }
 
-int main( void )
+
+
+void Led0Blink( void *pvParameters )
 {
-	unsigned int time_ctr = 0;
-	Led_Init();
-	
 	while(1)
 	{
-		if(time_ctr % 2 == 0) 	Led_Toggle(1);
-		if(time_ctr % 5 == 0)	Led_Toggle(0);
-		
-		time_ctr++;
-		Delay(50);
+		Led_Toggle(0);
+		Delay(500);
 	}
 }
+
+
+void Led1Blink( void *pvParameters )
+{
+	while(1)
+	{
+		Led_Toggle(1);
+		Delay(500);
+	}
+}
+
+
+int main(void)
+{
+	Led_Init();
+	xTaskCreate(Led0Blink, NULL , 100 , NULL, 2 , NULL );
+	xTaskCreate(Led1Blink, NULL , 100 , NULL, 2 , NULL );
+	vTaskStartScheduler();
+	while(1);
+}
+
