ce0a0bbd51ebebce14f5576eac3c13f92e1a0f31
PawelZieba01 <zpawel.zieba@gmail.com>
2024-05-27 19:03:42 +0200

    Skończone zadanie 4.2.1.1

--- Demo/ARM7_LPC2129_Keil_RVDS/main.c
+++ Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -5,17 +5,31 @@
 #include "uart.h"
 #include "keyboard.h"
 #include "string.h"
+#include "queue.h"
 
-xSemaphoreHandle xSemaphore;
+QueueHandle_t xUartQueue;
+
+typedef struct sMessage
+{
+	char cString[32];
+} tMessage;
 
 unsigned int uiTime = 0;
 
-void Rtos_Transmiter_SendString (char cString[])
+
+
+void Rtos_Transmiter_SendString(void *pvParameters)
 {		
-	xSemaphoreTake(xSemaphore, portMAX_DELAY);
-	Transmiter_SendString(cString);
-	while (eTransmiter_GetStatus()!=FREE){};
-	xSemaphoreGive(xSemaphore);	
+	while(1)
+	{
+		tMessage xMessage;
+		if(xQueueReceive(xUartQueue, &xMessage, portMAX_DELAY) == pdTRUE)
+		{
+			Transmiter_SendString(xMessage.cString);
+			while (eTransmiter_GetStatus()!=FREE){};
+		}
+		vTaskDelay(300);
+	}
 }
 
 
@@ -23,15 +37,15 @@ void Rtos_Transmiter_SendString (char cString[])
 void LettersTx (void *pvParameters){
 	while(1)
 	{
-		char cTimeString[16] = "-ABCDEEFGH-:";
+		tMessage xMessage = {"-ABCDEEFGH-:"};
 		unsigned long uiStartTime; 
 		unsigned long uiStopTime;
 		
-		AppendUIntToString(uiTime, cTimeString);
-		AppendString("\n", cTimeString);
+		AppendUIntToString(uiTime, xMessage.cString);
+		AppendString("\n", xMessage.cString);
 		
 		uiStartTime = xTaskGetTickCount();
-		Rtos_Transmiter_SendString(cTimeString);
+		xQueueSend(xUartQueue, xMessage.cString, portMAX_DELAY);
 		uiStopTime = xTaskGetTickCount();
 		
 		uiTime = uiStopTime - uiStartTime;
@@ -44,7 +58,8 @@ void KeyboardTx (void *pvParameters){
 	while(1){
 		if(eKeyboardRead() != RELASED)
 		{
-			Rtos_Transmiter_SendString("-Keyboard-\n");
+			tMessage xMessage = {"-Keyboard-\n"};
+			xQueueSend(xUartQueue, &xMessage, portMAX_DELAY);
 		}
 		vTaskDelay(300);
 	}
@@ -56,10 +71,11 @@ int main( void ){
 	UART_InitWithInt(300);
 	KeyboardInit();
 	
-	vSemaphoreCreateBinary( xSemaphore );
+	xUartQueue = xQueueCreate(5, sizeof(tMessage));
 	
 	xTaskCreate(LettersTx, NULL, 128, NULL, 1, NULL );
 	xTaskCreate(KeyboardTx, NULL, 128, NULL, 1, NULL );
+	xTaskCreate(Rtos_Transmiter_SendString, NULL, 128, NULL, 1, NULL );
 	
 	vTaskStartScheduler();
 	while(1);
